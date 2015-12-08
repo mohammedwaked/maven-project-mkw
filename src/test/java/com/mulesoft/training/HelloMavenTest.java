69d4e6920eca7d565503bb1d5b8417f8065452ff
Mohammed Waked <mohammed.waked@bitsinglass.com>
2015-12-08 15:13:34 -0700

    Initial commit

---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,17 @@
+package com.mulesoft.training;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("maven-project:hello", "Hello Maven");
+    }
+    
+    @Override
+    protected String getConfigFile() {
+        return "maven-project.xml";
+    }
+}
